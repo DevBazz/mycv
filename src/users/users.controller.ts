d3809de112b5a72b6d5b11f435f3dd111b1b852d
DevBazz <bazaniqbal@gmail.com>
2026-02-05 20:04:17 +0500

    create and save user using typeorm

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable prettier/prettier */
 import { Body, Controller, Post } from '@nestjs/common';
 import { createUserDto } from './dtos/create-user-dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService){}
   @Post('/signup')
    createUser(@Body() body: createUserDto){
-    console.log(body)
+    
+     this.userService.create(body.email, body.password)
    }
 }
